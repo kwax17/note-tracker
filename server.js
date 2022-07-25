b4f5792c3d00422f5fcb71ce068df4c49ba80f82
Kelly Wacker <kelwacker@gmail.com>
2022-07-25 10:10:45 -0400

    routes

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+// dependencies
 const express = require('express');
 const fs = require('fs');
 
+// server
 const PORT = process.env.PORT || 3002;
 const app = express();
-
+// data array
 const { notes } = require('./db/db.json');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// middleware
+app.use(express.static('public'));
+
+// gets notes from array // API ROUTE
 app.get('/api/notes', (req, res) => {
     fs.readFile("./db/db.json").then(function(data) {
         notes = [].concat(JSON.parse(data))
@@ -13,12 +21,34 @@ app.get('/api/notes', (req, res) => {
     })
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
+// posting and saving note to data array // API ROUTE
+app.post('/api/notes', (req, res) => {
+    const note = req.body;
+    fs.readFile('./db/db.json').then(function(data){
+        notes = [].concat(JSON.parse(data));
+        note.id = notes.length + 1
+        notes.push(note);
+        return notes
+    }).then(function(notes) {
+        fs.writeFile('./db/db.json', JSON.stringify(notes))
+        res.json(notes);
+    })
+});
 
-app.use('/', htmlRoutes);
+// gets the note html file // HTML ROUTE
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+// gets the note html file // HTML ROUTE
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+// gets the note html file // HTML ROUTE
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+// listening for port
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
