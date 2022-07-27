160a8c28e6dbfc231e90e4aa8d8833f8f5b1f805
Kelly Wacker <kelwacker@gmail.com>
2022-07-26 22:54:35 -0400

    chaos

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // dependencies
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+const { writeNote } = require("./lib/notes")
 
 // server
 const PORT = process.env.PORT || 3002;
@@ -13,42 +15,42 @@ app.use(express.json());
 // middleware
 app.use(express.static('public'));
 
+
 // gets notes from array // API ROUTE
 app.get('/api/notes', (req, res) => {
-    fs.readFile("./db/db.json").then(function(data) {
-        notes = [].concat(JSON.parse(data))
-        res.json(notes);
-    })
+    let results = notes;
+    if (req) {
+        console.log(notes);
+    };
+    res.json(results);
 });
 
 // posting and saving note to data array // API ROUTE
 app.post('/api/notes', (req, res) => {
-    const note = req.body;
-    fs.readFile('./db/db.json').then(function(data){
-        notes = [].concat(JSON.parse(data));
-        note.id = notes.length + 1
-        notes.push(note);
-        return notes
-    }).then(function(notes) {
-        fs.writeFile('./db/db.json', JSON.stringify(notes))
-        res.json(notes);
-    })
+    const note = writeNote(req.body, notes);
+    res.json(note);
 });
 
 // gets the note html file // HTML ROUTE
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-// gets the note html file // HTML ROUTE
+// gets the index page file // HTML ROUTE
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-// gets the note html file // HTML ROUTE
-app.get('/', (req, res) => {
+// gets the index page for everything besides the notes request // HTML ROUTE
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// listening for port
+app.get("/db", (req, res) => {
+    let results = db;
+    res.json(results);
+});
+
+
+// listening at port
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
