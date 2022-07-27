a9835d163507d341429817d9f92f21d657eaf129
Kelly Wacker <kelwacker@gmail.com>
2022-07-26 23:14:46 -0400

    yes! it saves to json

--- server.js
+++ server.js
@@ -15,38 +15,52 @@ app.use(express.json());
 // middleware
 app.use(express.static('public'));
 
+// gets the note html file // HTML ROUTE
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+// gets the index page file // HTML ROUTE
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+// gets the index page for everything besides the notes request // HTML ROUTE
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 // gets notes from array // API ROUTE
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    if (req) {
-        console.log(notes);
-    };
-    res.json(results);
-});
+app.get('/api/notes', (req, res) => res.json(notes));
 
 // posting and saving note to data array // API ROUTE
 app.post('/api/notes', (req, res) => {
-    const note = writeNote(req.body, notes);
-    res.json(note);
-});
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
 
-// gets the note html file // HTML ROUTE
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-// gets the index page file // HTML ROUTE
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-// gets the index page for everything besides the notes request // HTML ROUTE
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+    // break down request
+    const { title, text } = req.body;
 
-app.get("/db", (req, res) => {
-    let results = db;
-    res.json(results);
+    if (title && text) {
+    const newNote = {
+      title, 
+      text
+    };
+
+    // Convert the data to a string so we can save it
+    const reviewString = JSON.stringify(newNote);
+
+    // Write the string to a file
+    fs.writeFile(`./db/db.json`, reviewString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `Note for ${newNote.title} has been written to JSON file`
+          )
+    );
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
 });
 
 
